5e9ccb3044d70cc764d4cdcc4f49009bf993ae49
Dewey Hong <dhong@fortinet.com>
2017-07-25 09:19:37 -0700

    .

--- build.sh
+++ build.sh
@@ -1,17 +1,14 @@
 #!/usr/bin/env bash
 
-set -x
-
 if [ ! -d "tz-spring-boot" ]; then
-	echo "run this on this folder!"
 	echo "git clone https://github.com/doohee323/tz-spring-boot.git"
-	exit -1
+	git clone https://github.com/doohee323/tz-spring-boot.git
 fi
 
 if [ ! -d "tz-angular1.4" ]; then
 	echo "run this on this folder!"
 	echo "git clone https://github.com/doohee323/tz-angular1.4.git"
-	exit -1
+	git clone https://github.com/doohee323/tz-angular1.4.git
 fi
 
 vagrant destroy -f && vagrant up
