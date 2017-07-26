98c7a8c53f3caaeea6f247731a3799feb3ebcd5d
Dewey Hong <dhong@fortinet.com>
2017-07-26 15:08:09 -0700

    fix

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,10 +8,12 @@ sudo chmod -Rf 777 /opt/tomcat/logs
 
 sudo rsync -avP /vagrant/tz-angular1.4/app/ /var/www/html/ 
 sudo sed -i "s/localhost:8080/dev.tz.com/g" /var/www/html/scripts/app.js
-sudo chown -Rf www-data:www-data /var/www/html
-sudo chmod -Rf 777 /var/www/html
 
+sudo cp -Rf /vagrant/tz-angular1.4/bower_components/ /var/www/html/bower_components/
 sudo cp -rf /vagrant/resources/nginx/default /etc/nginx/sites-available/default
 sudo cp -rf /vagrant/resources/nginx/nginx.conf /etc/nginx/nginx.conf
+
+sudo chown -Rf www-data:www-data /var/www/html
+sudo chmod -Rf 777 /var/www/html
 
 exit 0
--- scripts/ui.sh
+++ scripts/ui.sh
@@ -7,13 +7,12 @@ export PROJ_DIR=/vagrant
 ###############################################################
 # install node, bower, grunt, compass
 ###############################################################
-# for ubuntu 14.04
-#sudo apt install nodejs-legacy -y
+sudo apt install nodejs-legacy -y
 sudo apt install npm -y
 
 sudo npm install -g bower -y
-sudo npm install grunt --save-dev
-sudo npm install grunt-contrib-jshint --save-dev
+sudo npm install grunt -g
+sudo npm install grunt-contrib-jshint -g
 sudo apt-get install ruby-compass -y
 sudo gem install compass
 
@@ -26,8 +25,7 @@ sudo mkdir -p /var/www/html
 cd $PROJ_DIR/tz-angular1.4
 
 npm install
-bower install
-cp -Rf $PROJ_DIR/tz-angular1.4/bower_components $PROJ_DIR/tz-angular1.4/app/bower_components
+bower install --allow-root
 #sudo chown -Rf ubuntu:ubuntu *
 
 #curl http://192.168.82.170
